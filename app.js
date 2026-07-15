01122a6293c24a2be78864323bbab62b174969f1
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-16 01:57:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// const temp = 10
+// console.log(temp);
+
+// let temp= 10
+// temp =100;
+// console.log(temp);
+
+// console.log(temp);
+// var temp = 10;
+
+// questions
+
+// let a= 4
+// let b= 9
+// console.log(a+b)
+
+// let s= "hello"
+// let temp= 12
+// console.log(s+temp);
+
+// let a= 10
+// let b = 20
+// // console.log(" the sum of "+ a +" and "+b +" is "+(a+b));
+// console.log(a+b +" is the sum of" + a +" and "+b); // bcoz the js engine works like that 
+
+// let prompt =require('prompt-sync')();
+// let age = prompt ("enter your age ")
+
+// console.log(`age is ${age}`);
+
+// swap methods
+// 1
+
+// let a= 10
+// let b= 20
+
+// let temp= a;
+// a=b;
+// b= temp;
+
+// // console.log(a,b);
+// console.log(a);
+// console.log(b);
+
+// 2
+
+// a= a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log(a);
+// console.log(b);
+
+// 3
+// console.log(a,b);
+// [a,b]=[b,a];
+// console.log(a,b);
