3672dfb06b1ade84ec5cf3cf13bd957f61d9e620
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-17 18:07:33 +0530

    starting if-else

---
+++ ifelse.js
@@ -0,0 +1,22 @@
+let prompt = require ('prompt-sync')();
+
+// let age = Number(prompt ("enter your age "));
+
+// if (age>=18){
+//     console.log("you are eligible for voting");
+// }
+// else{
+//     console.log("you are not eligible for voting");
+// }
+
+// let marks = Number(prompt("enter your marks "));
+
+// if (marks>90 && marks <100) console.log("A+");
+// else if (marks >80 && marks <=90) console.log ("A");
+// else if (marks >70 && marks <=80) console.log ("B");
+// else if (marks >60 && marks <=70) console.log ("C");
+// else if (marks >50 && marks <=60) console.log ("D");
+// else if (marks >40 && marks <=50) console.log ("E");
+// else if(marks >30 && marks <=40) console.log ("F");
+// else {console.log("dont come to school MF boyieee")}
+
