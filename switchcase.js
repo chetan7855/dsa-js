75d8d2644591d554933cf86760770736c2a40412
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-18 23:58:09 +0530

    topic- switch case

--- switchcase.js
+++ switchcase.js
@@ -20,22 +20,51 @@ let prompt = require ("prompt-sync")();
 // }
 
 
-let n = prompt("enter a string value: ");
-let consonent=0 , vowel=0;
+// let n = prompt("enter a string value: ");
+// let consonent=0 , vowel=0;
 
-for (let i=0; i < n.length; i++){
-    let ch= n.charAt(i);
-    switch(ch){
-        case 'a':
-        case 'e':
-        case 'i':
-        case 'o':
-        case 'u': vowel++;
+// for (let i=0; i < n.length; i++){
+//     let ch= n.charAt(i);
+//     switch(ch){
+//         case 'a':
+//         case 'e':
+//         case 'i':
+//         case 'o':
+//         case 'u': vowel++;
+//         break
+
+//         default : consonent++;
+//     }
+
+// }
+// console.log("consonent " + consonent );
+// console.log("vowel "+ vowel);
+
+console.log("enter 1 for area of rectangle");
+console.log("enter 2 for area of square");
+console.log("enter 3 for area of triangle");
+
+let n = Number(prompt ());
+
+
+switch(n){
+    case 1:{
+        let l=Number(prompt("enter length"))
+        let b = Number(prompt("enter breath"))
+        console.log(l*b);
         break
-
-        default : consonent++;
     }
 
+case 2:{
+       let side=Number(prompt("enter side"))
+       console.log(side * side);
+       break
 }
-console.log("consonent " + consonent );
-console.log("vowel "+ vowel);
+case 3:{
+       let height=Number(prompt("enter height"))
+        let base = Number(prompt("enter base"))
+        console.log((base *height)/2);
+        break
+}
+default: console.log("invaild");
+}
