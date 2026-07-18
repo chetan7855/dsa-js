df7a79bbb31458dd897caecbf044c4fa205030fb
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-19 00:13:44 +0530

    assign

--- switchcase.js
+++ switchcase.js
@@ -40,31 +40,49 @@ let prompt = require ("prompt-sync")();
 // console.log("consonent " + consonent );
 // console.log("vowel "+ vowel);
 
-console.log("enter 1 for area of rectangle");
-console.log("enter 2 for area of square");
-console.log("enter 3 for area of triangle");
+// console.log("enter 1 for area of rectangle");
+// console.log("enter 2 for area of square");
+// console.log("enter 3 for area of triangle");
 
-let n = Number(prompt ());
+// let n = Number(prompt ());
 
 
-switch(n){
-    case 1:{
-        let l=Number(prompt("enter length"))
-        let b = Number(prompt("enter breath"))
-        console.log(l*b);
-        break
-    }
+// switch(n){
+//     case 1:{
+//         let l=Number(prompt("enter length"))
+//         let b = Number(prompt("enter breath"))
+//         console.log(l*b);
+//         break
+//     }
 
-case 2:{
-       let side=Number(prompt("enter side"))
-       console.log(side * side);
-       break
-}
-case 3:{
-       let height=Number(prompt("enter height"))
-        let base = Number(prompt("enter base"))
-        console.log((base *height)/2);
-        break
-}
-default: console.log("invaild");
-}
+// case 2:{
+//        let side=Number(prompt("enter side"))
+//        console.log(side * side);
+//        break
+// }
+// case 3:{
+//        let height=Number(prompt("enter height"))
+//         let base = Number(prompt("enter base"))
+//         console.log((base *height)/2);
+//         break
+// }
+// default: console.log("invaild");
+// }
+
+// let a = Number (prompt("enter the number a "));
+// let b = Number (prompt("enter the number b "));
+// let c = Number (prompt("enter the number c "));
+
+
+// if (a>b && a>c ) console.log("a is greater ")
+// else if (b>a && b>c) console.log("b is greater")
+// else if (c>a && c>b) console.log("c is greater")
+// else if (a == b && a > c) {
+//     console.log("a and b are equal and greater than c");
+// }
+// else if (a == c && a > b) {
+//     console.log("a and c are equal and greater than b");
+// }
+// else if (b == c && b > a) {
+//     console.log("b and c are equal and greater than a");
+// }
