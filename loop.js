256661f75d169ea6e3a188f87819e036cfcc9915
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-20 23:40:05 +0530

    loop questions

--- loop.js
+++ loop.js
@@ -8,11 +8,22 @@ let prompt = require('prompt-sync')();
 //     console.log("hello world");
 // }
 
-let n = Number(prompt("enter number"));
-let count=0;
-for(let i=1;i<=n;i++){
-    if (n%i==0) count++;
+// let n = Number(prompt("enter number"));
+// let count=0;
+// for(let i=1;i<=n;i++){
+//     if (n%i==0) count++;
+// }
+
+// if (count==2) console.log("prime number");
+// else console.log("not prime number");
+
+let num = 12345;
+let reverse = 0;
+
+while (num > 0) {
+    let digit = num % 10;
+    reverse = reverse * 10 + digit;
+    num = Math.floor(num / 10);
 }
 
-if (count==2) console.log("prime number");
-else console.log("not prime number");
+console.log(reverse);
