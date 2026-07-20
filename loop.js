1ac08b70e7b72e2719447860815f51161fe33b73
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-20 23:37:28 +0530

    loop questions

--- loop.js
+++ loop.js
@@ -1,2 +1,18 @@
 //for 
 
+let prompt = require('prompt-sync')();
+
+// let n = Number (prompt("enter a number "))
+
+// for (let i =1 ; i <=n ; i++){
+//     console.log("hello world");
+// }
+
+let n = Number(prompt("enter number"));
+let count=0;
+for(let i=1;i<=n;i++){
+    if (n%i==0) count++;
+}
+
+if (count==2) console.log("prime number");
+else console.log("not prime number");
