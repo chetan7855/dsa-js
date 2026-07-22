24edd44e8a114e146cca3152dcb4da5e7482e5a6
CHETAN CHOUDHARY <83072891+chetan7855@users.noreply.github.com>
2026-07-22 23:36:45 +0530

    loop questions

--- loop.js
+++ loop.js
@@ -17,7 +17,19 @@ let prompt = require('prompt-sync')();
 // if (count==2) console.log("prime number");
 // else console.log("not prime number");
 
-let num = 12345;
+// let num = 12345;
+// let reverse = 0;
+
+// while (num > 0) {
+//     let digit = num % 10;
+//     reverse = reverse * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+
+// console.log(reverse);
+
+let num = 121;
+let original = num;
 let reverse = 0;
 
 while (num > 0) {
@@ -26,4 +38,8 @@ while (num > 0) {
     num = Math.floor(num / 10);
 }
 
-console.log(reverse);
+if (original === reverse) {
+    console.log("Palindrome");
+} else {
+    console.log("Not Palindrome");
+}
